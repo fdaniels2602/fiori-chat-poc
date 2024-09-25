cd51161eda2bf5716e69b979dba02cf3407330c5
fdaniels2602 <fuad2602@gmail.com>
2024-09-25 19:46:22 +0200

    add asynchronous processing of bot response

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -6,64 +6,10 @@ sap.ui.define([
 
         return Controller.extend("za.co.fuad.chatapp.controller.Home", {
 
-            _formatDate: function (date) {
-                const options = { day: '2-digit', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: false };
-                return date.toLocaleString('en-GB', options).replace(',', '');
-            },
-
-            onInit: function () {
-
-            },
-
-            onActionPressed: function (oEvent) {
-                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                const sMessagePath = oEvent.getSource().getParent().getBindingContext().getPath();
-                oRouter.navTo("citations", {
-                    messagePath: encodeURIComponent(sMessagePath)
-                });
-            },
-
-            onPost: function (oEvent) {
-                // Assume "myList" is the ID of your sap.m.List control
-                var oList = this.getView().byId("HomeList");
-
-                // Get the number of items in the list
-                var itemCount = oList.getItems().length;
-
-                // Scroll to the last item (index is zero-based, so we subtract 1)
-                if (itemCount > 0) {
-                    oList.scrollToIndex(itemCount - 1);
-                }
-
-                // get the parent component for the current view
-                const oComponent = this.getOwnerComponent();
-
-                // get the default JSON model
-                const oModel = oComponent.getModel();
-
-                // add a new entry to the end of the model
-                let oData = oModel.getData();
-
-                // get the value of the text of the feedinput UI control
-                const sValue = this.getView().byId("FeedInput").getValue();
-
-                // get current timestamp
-                const now = new Date();
-                const formattedDate = this._formatDate(now);
-
-                debugger;
-
-                oData.chat_history.push({
-                    "role": "human",
-                    "message": sValue,
-                    "timestamp": formattedDate,
-                    "citation_count": 0
-                });
-
-                debugger;
+            _addFact: function () {
 
                 // get a reference to the facts model
-                const oFactsModel = oComponent.getModel("facts");
+                const oFactsModel = this.oComponent.getModel("facts");
 
                 // get the array of facts from the facts model
                 const aFacts = oFactsModel.getData().black_hole_facts;
@@ -71,6 +17,13 @@ sap.ui.define([
                 // randomly read one of the entries in the aFacts array
                 const iRandomIndex = Math.floor(Math.random() * aFacts.length);
                 const randomFact = aFacts[iRandomIndex].fact;
+
+                // get the default JSON model
+                const oModel = this.oComponent.getModel();
+                let oData = oModel.getData();
+
+                const now = new Date();
+                const formattedDate = this._formatDate(now);
 
                 oData.chat_history.push({
                     "role": "bot",
@@ -83,8 +36,74 @@ sap.ui.define([
                 oModel.setData(oData);
 
                 // scroll the list to the last entry
-                oList.scrollToIndex(oData.chat_history.length - 1);
+                this.oList.scrollToIndex(oData.chat_history.length - 1);
 
+                // enable feed input control
+                this.oFeedInput.setEnabled(true);
+            },
+
+            _delayBySeconds: async function (seconds) {
+
+                var that = this;
+
+                return new Promise(function (resolve, reject) {
+                    setTimeout.call(this, function () {
+                        that._addFact();
+                    }, seconds * 1000);
+                });
+            },
+
+            _formatDate: function (date) {
+                const options = { day: '2-digit', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: false };
+                return date.toLocaleString('en-GB', options).replace(',', '');
+            },
+
+            onInit: function () {
+                this.oFeedInput = this.getView().byId("FeedInput");
+                this.oList = this.getView().byId("HomeList");
+                this.oComponent = this.getOwnerComponent();
+            },
+
+            onActionPressed: function (oEvent) {
+                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                const sMessagePath = oEvent.getSource().getParent().getBindingContext().getPath();
+                oRouter.navTo("citations", {
+                    messagePath: encodeURIComponent(sMessagePath)
+                });
+            },
+
+            onPost: async function (oEvent) {
+
+                // get the default JSON model
+                const oModel = this.oComponent.getModel();
+
+                // add a new entry to the end of the model
+                let oData = oModel.getData();
+
+                // get the value of the text of the feedinput UI control
+                //const oFeedInput = this.getView().byId("FeedInput");
+                const sValue = this.oFeedInput.getValue();
+
+                // disable feed input control
+                this.oFeedInput.setEnabled(false);
+
+                // get current timestamp
+                const now = new Date();
+                const formattedDate = this._formatDate(now);
+
+                oData.chat_history.push({
+                    "role": "human",
+                    "message": sValue,
+                    "timestamp": formattedDate,
+                    "citation_count": 0
+                });
+
+                oModel.setData(oData);
+
+                this.oList.scrollToIndex(oData.chat_history.length - 1);
+
+                //simulate wait for bot to respond
+                await this._delayBySeconds(2);
 
             }
 
